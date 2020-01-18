5611957021fc2d6f553d5dfd70e19a1acebc9cc3
MHane952 <haneymason22@gmail.com>
2020-01-18 17:39:24 -0600

    friends list and routing functional

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Brian Cogman",
+        photo: "",
+        score: [
+            "5",
+            "3",
+            "2",
+            "4",
+            "3",
+            "5",
+            "1",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        name: "James Rolffe",
+        photo: "",
+        score: [
+            "3",
+            "2",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Lowell Granger",
+        photo: "",
+        score: [
+            "1",
+            "3",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "3",
+            "2",
+            "5"
+        ]
+    }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -7,41 +7,10 @@ var PORT = 1313
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var friends = {
-  name: "",
-  photo: "",
-  scores: [""]
-}
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-  });
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-  });
-  
-  
-
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
-  
-
-app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-  
-    
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newFriend);
-  
-    characters.push(newFriend);
-  
-    res.json(newFriend);
-  });
-
-  app.listen(PORT, function() {
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
   
